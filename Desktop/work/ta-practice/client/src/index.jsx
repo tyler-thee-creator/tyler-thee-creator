db7507bb63d985f0def14d3e8fa91559bbbd3a51
Tyler Brown <tyler.s.brown.93@gmail.com>
2020-12-12 18:08:21 -0800

    Set up webpack

---
+++ Desktop/work/ta-practice/client/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>This is an app</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById(''));
---
+++ Desktop/work/ta-practice/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(express.static(__dirname + '/../client/src'));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('hello word');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
