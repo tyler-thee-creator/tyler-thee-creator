90c366e0541c77282f390dfa0d197d8f11f2591c
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-06-14 19:21:08 -0700

    Add partitionList TP

---
+++ Desktop/code/coding-challenges/partitionList/partitionList.js
@@ -0,0 +1,19 @@
+/*https://leetcode.com/problems/partition-list*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+ const partition = (head, x) => {
+
+}
+
+export default partition;
